1204190a04824c8b6de901438e9fa702da309c5a
Sr. Blue <91746122+Deluwuta@users.noreply.github.com>
2023-12-06 00:07:24 +0100

    Add files via upload

---
+++ day1/day1.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+fn main() {
+    println!("Hola mama!");
+    let input = fs::read_to_string("input.txt")
+                .expect("Deberia haber podido leer el fichero owo");
+    let mut aux_val = 0;
+
+    for word in input.split("\n") {
+        for char in word.chars() {
+            if char.is_numeric() {
+                aux_val += char.to_digit(10).unwrap() * 10;
+                break;
+            }
+        }
+        for char in word.chars().rev() {
+            if char.is_numeric() {
+                aux_val += char.to_digit(10).unwrap();
+                break;
+            }
+        }
+    }
+    println!("La suma es {aux_val}");
+}
